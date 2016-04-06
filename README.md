ff1dae65918da3e597cb4963511836584660224e
Paul Warren <paul.warren@emc.com>
2016-04-06 08:55:09 -0700

    Update readme
    [#112362783] https://www.pivotaltracker.com/story/show/112362783
    
    Signed-off-by: Michael Maximilien <maxim@us.ibm.com>

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 *Tested on bosh-lite and aws*
 
+## Overview
+
+This bosh release comprises two jobs; cephfs and cephdriver.  These are typically deployed separately.  
+
 ## Installation
 ### Pre-Requisites
 ```
@@ -30,9 +34,18 @@ Upload the release to your bosh director.
 bosh upload release
 ```
 
+#### To deploy Cephfs
+
 Generate the manifest for your environment.  Check your `bosh target` is correct.
 ```
 ./templates/generate_manifest.sh bosh-lite|aws
+```
+
+##### To deploy Cephdriver
+
+Generate the manifest for your environment.  Check your `bosh target` is correct.
+```
+./templates/generate_manifest.sh bosh-lite|aws cephdriver
 ```
 
 And deploy:-
@@ -42,6 +55,8 @@ bosh deploy
 NB: When prompted, answer `yes`.
 
 ## Verify the install
+
+#### Cephfs
 
 SSH onto the bosh release VM
 
@@ -65,6 +80,37 @@ cluster c0162a84-1d21-46a2-8a8e-4507f6ec707f
         51265 MB used, 151 GB / 206 GB avail
              320 active+clean
 ```
+
+#### Cephfs
+
+SSH onto the bosh release VM
+
+`bosh ssh --gateway_host <your bosh director ip> --gateway_user vcap --strict_host_key_checking=no cephdriver/0`
+
+Check the health of the cephdriver service
+
+`monit summary`
+
+which should report something like this:-
+
+```
+The Monit daemon 5.2.4 uptime: 15h 24m
+
+Process 'cephdriver'                running
+System 'system_50b08287-989a-4e81-8c0e-9c22d5cc809e' running
+```
+NB: You will probably need to be root to do this
+
+You can also curl an endpoint:-
+
+`curl http://localhost:8080/get`
+
+which should report something like this:-
+```
+{"Volume":{"Name":"","Mountpoint":""},"Err":"unexpected end of JSON input"}
+```
+NB: Whilst this is an error it proves the server is up and listening for requests.  8080 is the default port.  You may need to replace this with the 
+port you specified in your deployment manifest.
 
 ## Mount cephfs (using fuse)
 
--- templates/generate_manifest.sh
+++ templates/generate_manifest.sh
@@ -3,36 +3,80 @@
 
 director_uuid=$(bosh status --uuid)
 
+usage () {
+    echo "Usage: generate_manifest.sh bosh-lite|aws [cephfs*|cephdriver|both]"
+    echo " * default"
+    exit 1
+}
+
 if [[  "$1" != "bosh-lite" && "$1" != "aws" ]]
   then
-    echo "Usage: generate_manifest.sh bosh-lite|aws"
-    exit 1
+    usage
 fi
 
-ARG2=${2:-cephfs-bosh-release}
+ARG2=cephfs-bosh-release
 
-SUBSTITUTION=$(cat <<END_HEREDOC
+DIRECTOR_YML=$(cat <<END_HEREDOC
 name: ${ARG2}
 director_uuid: ${director_uuid}
 releases:
-  - name: ${ARG2}
-jobs:
-  - name: cephfs
-    templates:
-      - {release: ${ARG2}, name: cephfs}
+    - name: ${ARG2}
 END_HEREDOC
 )
 
-echo $SUBSTITUTION
-
-if [ "$1" == "bosh-lite" ] 
-  then
-    spiff merge templates/cephfs-manifest-boshlite.yml <(echo "${SUBSTITUTION}") > cephfs-manifest.yml
+if [ "$2" == "cephfs" -o "$2" == "" ]
+    then
+    MANIFEST_NAME=cephfs-manifest
+CEPHFS_JOB_YML=$(cat <<END_HEREDOC
+jobs:
+- name: cephfs
+  templates:
+  - {release: ${ARG2}, name: cephfs}
+END_HEREDOC
+)
+elif [ "$2" == "cephdriver" ]
+    then
+    MANIFEST_NAME=cephdriver-manifest
+CEPHDRIVER_JOB_YML=$(cat <<END_HEREDOC
+jobs:
+- name: cephdriver
+  templates:
+  - {release: ${ARG2}, name: cephdriver}
+END_HEREDOC
+)
+elif [ "$2" == "both" ]
+    then
+    MANIFEST_NAME=ceph-manifest
+CEPHFS_AND_CEPHDRIVER_JOB_YML=$(cat <<END_HEREDOC
+jobs:
+- name: cephfs
+  templates:
+  - {release: ${ARG2}, name: cephfs}
+- name: cephdriver
+  templates:
+  - {release: ${ARG2}, name: cephdriver}
+END_HEREDOC
+)
+else
+    usage
 fi
 
-if [ "$1" == "aws" ] 
+SUBSTITUTION=$(cat <<END_HEREDOC
+${DIRECTOR_YML}
+${CEPHFS_JOB_YML}
+${CEPHDRIVER_JOB_YML}
+${CEPHFS_AND_CEPHDRIVER_JOB_YML}
+END_HEREDOC
+)
+
+if [ "$1" == "bosh-lite" ]
   then
-    spiff merge templates/cephfs-manifest-aws.yml <(echo "${SUBSTITUTION}") > cephfs-manifest.yml
+    spiff merge templates/cephfs-manifest-boshlite.yml <(echo "${SUBSTITUTION}") > $MANIFEST_NAME.yml
 fi
 
-bosh deployment cephfs-manifest.yml
+if [ "$1" == "aws" ]
+  then
+    spiff merge templates/cephfs-manifest-aws.yml <(echo "${SUBSTITUTION}") > $MANIFEST_NAME.yml
+fi
+
+bosh deployment $MANIFEST_NAME.yml
