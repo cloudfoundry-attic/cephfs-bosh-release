f5270cba4867cb7fb3e32e2cb0f6534c5c1b8928
Luke Woydziak <luke.woydziak@emc.com>
2016-04-11 17:04:03 -0700

    wip

--- templates/generate_manifest.sh
+++ templates/generate_manifest.sh
@@ -4,7 +4,7 @@
 director_uuid=$(bosh status --uuid)
 
 usage () {
-    echo "Usage: generate_manifest.sh bosh-lite|aws [cephfs*|cephdriver|both]"
+    echo "Usage: generate_manifest.sh bosh-lite|aws cephfs*|cephdriver|both <job_ip> <cephfs_ip> <drivers_path> <keyring>"
     echo " * default"
     exit 1
 }
@@ -14,52 +14,71 @@ if [[  "$1" != "bosh-lite" && "$1" != "aws" ]]
     usage
 fi
 
-ARG2=cephfs-bosh-release
-
-DIRECTOR_YML=$(cat <<END_HEREDOC
-name: ${ARG2}
-director_uuid: ${director_uuid}
-releases:
-    - name: ${ARG2}
-END_HEREDOC
-)
+release_name=cephfs-bosh-release
+deployment_name=$2
+job_ip=$3
 
 if [ "$2" == "cephfs" -o "$2" == "" ]
     then
-    MANIFEST_NAME=cephfs-manifest
+    MANIFEST_FILENAME=cephfs-$1-manifest
 CEPHFS_JOB_YML=$(cat <<END_HEREDOC
 jobs:
 - name: cephfs
+  networks:
+  - name: ceph-net
+    static_ips:
+    - $job_ip
   templates:
-  - {release: ${ARG2}, name: cephfs}
+  - {release: ${release_name}, name: cephfs}
 END_HEREDOC
 )
 elif [ "$2" == "cephdriver" ]
     then
-    MANIFEST_NAME=cephdriver-manifest
+    cephfs_ip=$6
+    drivers_path=$4
+    cephfs_keyring=`cat $5`
+    MANIFEST_FILENAME=cephdriver-$1-manifest
 CEPHDRIVER_JOB_YML=$(cat <<END_HEREDOC
 jobs:
 - name: cephdriver
+  properties:
+      cephdriver:
+        drivers_path: $drivers_path
+        cephfs_ip: $cephfs_ip
+        cephfs_keyring: |
+          $cephfs_keyring
+  networks:
+  - name: ceph-net
+    static_ips:
+    - $job_ip
   templates:
-  - {release: ${ARG2}, name: cephdriver}
+  - {release: ${release_name}, name: cephdriver}
 END_HEREDOC
 )
 elif [ "$2" == "both" ]
     then
-    MANIFEST_NAME=ceph-manifest
+    MANIFEST_FILENAME=ceph-$1-manifest
 CEPHFS_AND_CEPHDRIVER_JOB_YML=$(cat <<END_HEREDOC
 jobs:
 - name: cephfs
   templates:
-  - {release: ${ARG2}, name: cephfs}
+  - {release: ${release_name}, name: cephfs}
 - name: cephdriver
   templates:
-  - {release: ${ARG2}, name: cephdriver}
+  - {release: ${release_name}, name: cephdriver}
 END_HEREDOC
 )
 else
     usage
 fi
+
+DIRECTOR_YML=$(cat <<END_HEREDOC
+name: ${deployment_name}
+director_uuid: ${director_uuid}
+releases:
+    - name: ${release_name}
+END_HEREDOC
+)
 
 SUBSTITUTION=$(cat <<END_HEREDOC
 ${DIRECTOR_YML}
@@ -71,12 +90,12 @@ END_HEREDOC
 
 if [ "$1" == "bosh-lite" ]
   then
-    spiff merge templates/cephfs-manifest-boshlite.yml <(echo "${SUBSTITUTION}") > $MANIFEST_NAME.yml
+    spiff merge templates/cephfs-manifest-boshlite.yml <(echo "${SUBSTITUTION}") > $MANIFEST_FILENAME.yml
 fi
 
 if [ "$1" == "aws" ]
   then
-    spiff merge templates/cephfs-manifest-aws.yml <(echo "${SUBSTITUTION}") > $MANIFEST_NAME.yml
+    spiff merge templates/cephfs-manifest-aws.yml <(echo "${SUBSTITUTION}") > $MANIFEST_FILENAME.yml
 fi
 
-bosh deployment $MANIFEST_NAME.yml
+bosh deployment $MANIFEST_FILENAME.yml
