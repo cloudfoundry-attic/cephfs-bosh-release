5752c841dc5e20675e38a01307ca11fc2565b076
Julian Hjortshoj <julian.hjortshoj@emc.com>
2018-02-09 14:16:08 -0800

    move ceph bosh 2.0 manifest and deployment scripts into cephfs-bosh-release and delete legacy bosh 1.0 scripts and manifests [#152947649](https://www.pivotaltracker.com/story/show/152947649)

--- bosh_e2e_aws.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-bosh delete deployment --force cephfs-bosh-release
-bosh delete release cephfs-bosh-release --force
-bosh create release --force
-bosh upload release
-bosh -n deploy
-say "done"
-bosh ssh --gateway_host 52.72.95.180 --gateway_user vcap --strict_host_key_checking=no cephfs/0 --default_password c1oudc0w
